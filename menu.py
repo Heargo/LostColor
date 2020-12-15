79b84f533bb47ab0f724cfe65abd9e267dba2dd6
TheSummer1502 <pajeanromain@gmail.com>
2020-12-16 00:00:06 +0100

    Modif de la class bouton
    Le constructeur bouton a des paramètre optionnel pour modifier plus en détail le bouton (police utilisé, largeur, hauteur...)

--- menu.py
+++ menu.py
@@ -4,24 +4,29 @@ from pygame import *
 
 class Bouton():
 
-    def __init__(self, x, y, text, color):
+    def __init__(self, x, y, text, color, center=True, width=300, height=75, font='fonts/No_Color.ttf',font_size=40):
         self.text = text
         self.color = color
 
-        self.image = pygame.Surface((300, 75))
+        self.image = pygame.Surface((width, height))
         self.image.fill(self.color)
 
         self.rect = self.image.get_rect()
 
-        font = pygame.font.Font('fonts/No_Color.ttf', 40)
+        font = pygame.font.Font(font, font_size)
         self.text_surface = font.render(text, True, (0,0,0))
         self.text_rect = self.text_surface.get_rect()
         self.text_rect.centerx = self.rect.centerx
         self.text_rect.centery = self.rect.centery
         self.image.blit(self.text_surface, self.text_rect)
 
-        self.rect.centerx = x
-        self.rect.centery = y
+        if center:
+            self.rect.centerx = x
+            self.rect.centery = y
+        else:
+            self.rect.x = x
+            self.rect.y = y
+
         self.filtre = pygame.Surface((self.rect.width, self.rect.height), SRCALPHA)
         self.filtre_rect = self.filtre.get_rect()
         self.filtre.fill((255,255,255,100))
